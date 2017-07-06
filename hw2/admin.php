fad429f504bc061eae35a6a522062940bd47bc9c
Büşra Gümüşel <busragumusel@gmail.com>
2017-07-07 00:54:43 +0300

    added admin.php

---
+++ hw2/admin.php
@@ -0,0 +1,59 @@
+class Admin {
+    protected $adminName;
+    protected $password;
+    protected $email;
+
+    /**
+     * @return mixed
+     */
+    public function getAdminName()
+    {
+        return $this->adminName;
+    }
+
+    /**
+     * @param mixed $adminName
+     */
+    public function setAdminName($adminName)
+    {
+        $this->adminName = $adminName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function deleteCustomer()
+    {
+
+    }
+
+}
