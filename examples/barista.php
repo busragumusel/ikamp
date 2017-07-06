b79ac68b7780fb86fffa465399fce0c7c14247c3
Büşra Gümüşel <busragumusel@gmail.com>
2017-07-07 01:27:22 +0300

    Update barista.php

--- examples/barista.php
+++ examples/barista.php
@@ -1,17 +1,6 @@
 //5th of July
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 class Barista {
     public function MakeMeCoffee(CoffeeMachine $machine, Cup $cup){
         return $machine->fill($cup);
