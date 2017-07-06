4b7876da68a41ad5e6225010d622de45265d63a4
Büşra Gümüşel <busragumusel@gmail.com>
2017-07-07 00:55:22 +0300

    added customer.php

---
+++ hw2/customer.php
@@ -0,0 +1,149 @@
+class Customer {
+    protected $userId;
+    protected $userName;
+    protected $password;
+    protected $customerName;
+    protected $address;
+    protected $email;
+    protected $creditCardInfo;
+    protected $phone;
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param mixed $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * @param mixed $userName
+     */
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCustomerName()
+    {
+        return $this->customerName;
+    }
+
+    /**
+     * @param mixed $customerName
+     */
+    public function setCustomerName($customerName)
+    {
+        $this->customerName = $customerName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreditCardInfo()
+    {
+        return $this->creditCardInfo;
+    }
+
+    /**
+     * @param mixed $creditCardInfo
+     */
+    public function setCreditCardInfo($creditCardInfo)
+    {
+        $this->creditCardInfo = $creditCardInfo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+}
