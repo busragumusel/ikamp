a3b1908b171e6465b949984c2b8ba8c30c3cdcaf
Büşra Gümüşel <busragumusel@gmail.com>
2017-07-07 00:56:51 +0300

    added basket.php

---
+++ hw2/basket.php
@@ -0,0 +1,37 @@
+  class Basket {
+    protected $userId;
+    protected $orderId;
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param mixed $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrderId()
+    {
+        return $this->orderId;
+    }
+
+    /**
+     * @param mixed $orderId
+     */
+    public function setOrderId($orderId)
+    {
+        $this->orderId = $orderId;
+    }
+
+}
