0ed3e6b65b0d1cb168c80a21d8f07e36b505c105
Büşra Gümüşel <busragumusel@gmail.com>
2017-07-07 00:55:55 +0300

    added restaurant.php

---
+++ hw2/restaurant.php
@@ -0,0 +1,70 @@
+class Restaurant {
+    protected $restaurantName;
+    protected $restaurantId;
+    protected $address;
+    protected $phone;
+
+    /**
+     * @return mixed
+     */
+    public function getRestaurantName()
+    {
+        return $this->restaurantName;
+    }
+
+    /**
+     * @param mixed $restaurantName
+     */
+    public function setRestaurantName($restaurantName)
+    {
+        $this->restaurantName = $restaurantName;
+    }
+    /**
+     * @return mixed
+     */
+    public function getRestaurantId()
+    {
+        return $this->restaurantId;
+    }
+
+    /**
+     * @param mixed $restaurantId
+     */
+    public function setRestaurantId($restaurantId)
+    {
+        $this->restaurantId = $restaurantId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+}
